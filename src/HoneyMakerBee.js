e7fbfd249c688eaba07847480bf30dd491f9f8f1
elizabethcan <elizabeth230@gmail.com>
2019-01-09 11:11:17 -0800

    adds honeyMakerBee class

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,24 @@
 var HoneyMakerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.job = 'make honey';
+  this.color = Object.getPrototypeOf(HoneyMakerBee.prototype).color;
+  this.food = Object.getPrototypeOf(HoneyMakerBee.prototype).food;
+  this.honeyPot = 0;
 };
 
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+HoneyMakerBee.prototype.eat = function() {
+  Object.getPrototypeOf(HoneyMakerBee.prototype).eat;
+};
+
+HoneyMakerBee.prototype.makeHoney = function() {
+  this.honeyPot += 1;
+};
+
+HoneyMakerBee.prototype.giveHoney = function() {
+  this.honeyPot -= 1;
+};
