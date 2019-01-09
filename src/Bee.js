f57b35601c684bdd853606b9df6a7d479a8204e8
elizabethcan <elizabeth230@gmail.com>
2019-01-09 11:01:56 -0800

    adds Bee subclass

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,15 @@
 var Bee = function() {
+  Grub.call(this);
+  this.age = 5;
+  this.color = 'yellow';
+  this.food = Object.getPrototypeOf(Bee.prototype).food; // inherit from grub
+  this.job = 'keep on growing';
 };
 
+Bee.prototype = Object.create(Grub.prototype);
+
+Bee.prototype.constructor = Bee;
+
+Bee.prototype.eat = function() {
+  Object.getPrototypeOf(Bee.prototype).eat;
+}; // inherit from grub
